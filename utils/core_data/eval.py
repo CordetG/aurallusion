817ab702bcbdeea8d804a1967218cd0b08b8be12
Cordet Gula <cgula@pdx.edu>
2025-03-01 21:45:51 -0800

    create outline for data module

---
+++ utils/core_data/eval.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# saves frequency input from ui or input from backend
+def save_input():
+    pass
+# end def
+
+# TODO: Move from sound.py
+# pass an equation for producing samples
+# returns array of sample values
+def generate_wave_samples():
+    pass
