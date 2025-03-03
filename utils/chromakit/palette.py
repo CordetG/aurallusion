f3640665437b30f3321ed5f21041071fd70cb118
Cordet Gula <cgula@pdx.edu>
2025-03-03 02:49:05 -0800

    add palette methods

--- utils/chromakit/palette.py
+++ utils/chromakit/palette.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
 
+from .color import *
+from .default_colors import *
+import numpy as np
+import colorsys
 
-
-class Palette:
-    pass
+class ColorPalette:
+    '''Create an array of colors.
+    -----------
+    Attributes:
+        colors -- list of colors represented as (r,g,b) values
+    '''
+    
+    def __init__(self) -> None:
+        self.colors: list[int] = []
+    # end __init__ def
+    
+    def add_base_colors():
+        pass
+    # end def
+    
+    def add_greyscale():
+        pass
+    # end def
+    
+    def add_scaled_colors():
+        pass
+    # end def
+    
+    def copy_color_vectors():
+        pass
+    # end def
+    
 # end class
