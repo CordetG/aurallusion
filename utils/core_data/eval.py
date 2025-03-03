76cd535339520c33e30f7fe7a99282a995267d4e
Cordet Gula <cgula@pdx.edu>
2025-03-02 17:57:33 -0800

    update data methods for tree

--- utils/core_data/eval.py
+++ utils/core_data/eval.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 # saves frequency input from ui or input from backend
 def save_input():
     pass
@@ -12,11 +15,26 @@ def generate_wave_samples():
     pass
 
 # --- manage data for tree ---
-def import_data():
-    pass
+def load_data(csv_file) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(csv_file)
+        
+        return df
+        
+    except pd.errors.EmptyDataError:
+        print(f"Error: The file '{csv_file}' is empty or cannot be read.")
+        
+    return None
 # end def
 
-def split_dataset():
-    pass
+def split_dataset(data_frame):
+    
+    x = data_frame.values[1:, 3:7]
+    y = data_frame.values[1:, 0:3]
+    
+    x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.1, random_state=100)
+    
+    return x, y, x_train, x_test, y_train, y_test 
 # end def
 # -------
