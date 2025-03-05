83cc106f051bcd40795d7ecff60b98628ec16ca3
Cordet <115206755+CordetG@users.noreply.github.com>
2025-03-04 18:28:52 -0800

    Add UI Setup Functions (#19)
    * create outline methods fro ui
    
    * add read input method

--- ui/input_window.py
+++ ui/input_window.py
@@ -1,2 +1,35 @@
 #!/usr/bin/python3
+
 from tkinter import *
+
+# Set options for creating a gui window
+def create_window():
+    pass
+# end def
+
+def display_window_title():
+    pass
+# end def
+
+# Display specifications for input
+def display_specs():
+    pass
+# end def
+
+def display_input_text():
+    pass
+# end def
+
+# Make sure input is valid
+def input_error():
+    pass
+# end def
+
+# Determine out-of-bounds values
+def input_range():
+    pass
+# end def
+
+def read_input():
+    pass
+# end def
